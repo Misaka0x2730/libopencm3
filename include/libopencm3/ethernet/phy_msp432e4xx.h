3be408a188323ec44ad491ac843ac5513fbb9de2
Dmitry Rezvanov <dmitry.rezvanov@yandex.ru>
2018-09-28 18:01:34 +0400

    Add phy register (0x0 - 0x10) description.

--- include/libopencm3/ethernet/phy_msp432e4xx.h
+++ include/libopencm3/ethernet/phy_msp432e4xx.h
@@ -1,228 +1,200 @@
 #ifndef PHY_MSP432E4_H
 #define PHY_MSP432E4_H
 
-#define PHY_MSP432_EPHYBMCR         (0x00)
-#define PHY_MSP432_EPHYBMSR         (0x01)
-#define PHY_MSP432_EPHYID1          (0x02)
-#define PHY_MSP432_EPHYID2          (0x03)
-#define PHY_MSP432_EPHYANA          (0x04)
-#define PHY_MSP432_EPHYANLPA        (0x05)
-#define PHY_MSP432_EPHYANER         (0x06)
-#define PHY_MSP432_EPHYANNPTR       (0x07)
-#define PHY_MSP432_EPHYANLNPTR      (0x08)
+/**  */
+#define PHY_MSP432_ADDRESS          (PHY0)
+
+/** Ethernet PHY Basic Mode Control */
+#define PHY_MSP432_EPHYBMCR         (0x0)
+/** Ethernet PHY Basic Mode Status */
+#define PHY_MSP432_EPHYBMSR         (0x1)
+/** Ethernet PHY Identifier Register 1 */
+#define PHY_MSP432_EPHYID1          (0x2)
+/** Ethernet PHY Identifier Register 2 */
+#define PHY_MSP432_EPHYID2          (0x3)
+/** Ethernet PHY Auto-Negotiation Advertisement */
+#define PHY_MSP432_EPHYANA          (0x4)
+/** Ethernet PHY Auto-Negotiation Link Partner Ability */
+#define PHY_MSP432_EPHYANLPA        (0x5)
+/** Ethernet PHY Auto-Negotiation Expansion */
+#define PHY_MSP432_EPHYANER         (0x6)
+/** Ethernet PHY Auto-Negotiation Next Page TX */
+#define PHY_MSP432_EPHYANNPTR       (0x7)
+/** Ethernet PHY Auto-Negotiation Link Partner Ability Next Page */
+#define PHY_MSP432_EPHYANLNPTR      (0x8)
+/** Ethernet PHY Configuration 1 */
 #define PHY_MSP432_EPHYCFG1         (0x9)
+/** Ethernet PHY Configuration 2 */
 #define PHY_MSP432_EPHYCFG2         (0xA)
+/** Ethernet PHY Configuration 3 */
 #define PHY_MSP432_EPHYCFG3         (0xB)
+/** Ethernet PHY Register Control */
 #define PHY_MSP432_EPHYREGCTL       (0xD)
+/** Ethernet PHY Address or Data */
 #define PHY_MSP432_EPHYADDAR        (0xE)
+/** Ethernet PHY Status */
 #define PHY_MSP432_EPHYSTS          (0x10)
+/** Ethernet PHY Specific Control */
 #define PHY_MSP432_EPHYSCR          (0x11)
+/** Ethernet PHY MII Interrupt Status 1 */
 #define PHY_MSP432_EPHYMISR1        (0x12)
+/** Ethernet PHY MII Interrupt Status 2 */
 #define PHY_MSP432_EPHYMISR2        (0x13)
+/** Ethernet PHY False Carrier Sense Counter */
 #define PHY_MSP432_EPHYFCSCR        (0x14)
+/** Ethernet PHY Receive Error Count */
 #define PHY_MSP432_EPHYRXERCNT      (0x15)
+/** Ethernet PHY BIST Control */
 #define PHY_MSP432_EPHYBISTCR       (0x16)
+/** Ethernet PHY LED Control */
 #define PHY_MSP432_EPHYLEDCR        (0x18)
+/** Ethernet PHY Control */
 #define PHY_MSP432_EPHYCTL          (0x19)
+/** Ethernet PHY 10Base-T Status/Control */
 #define PHY_MSP432_EPHY10BTSC       (0x1A)
+/** Ethernet PHY BIST Control and Status 1 */
 #define PHY_MSP432_EPHYBICSR1       (0x1B)
+/** Ethernet PHY BIST Control and Status 2 */
 #define PHY_MSP432_EPHYBICSR2       (0x1C)
+/** Ethernet PHY Cable Diagnostic Control */
 #define PHY_MSP432_EPHYCDCR         (0x1E)
+/** Ethernet PHY Reset Control */
 #define PHY_MSP432_EPHYRCR          (0x1F)
+/** Ethernet PHY LED Configuration */
 #define PHY_MSP432_EPHYLEDCFG       (0x25)
 
-#define PHY_MSP432_EPHYSTS_LINK     (1 << 0)
-#define PHY_MSP432_EPHYSTS_SPEED    (1 << 1)
-#define PHY_MSP432_EPHYSTS_DUPLEX   (1 << 2)
+#define PHY_MSP432_EPHYBMCR_MIIRESET        (1 << 15)
+#define PHY_MSP432_EPHYBMCR_MIILOOPBK       (1 << 14)
+#define PHY_MSP432_EPHYBMCR_SPEED           (1 << 13)
+#define PHY_MSP432_EPHYBMCR_ANEN            (1 << 12)
+#define PHY_MSP432_EPHYBMCR_PWRDWN          (1 << 11)
+#define PHY_MSP432_EPHYBMCR_ISOLATE         (1 << 10)
+#define PHY_MSP432_EPHYBMCR_RESTARTAN       (1 << 9)
+#define PHY_MSP432_EPHYBMCR_DUPLEXM         (1 << 8)
+#define PHY_MSP432_EPHYBMCR_COLLTST         (1 << 7)
 
+#define PHY_MSP432_EPHYBMSR_100BTXFD        (1 << 14)
+#define PHY_MSP432_EPHYBMSR_100BTXHD        (1 << 13)
+#define PHY_MSP432_EPHYBMSR_10BTFD          (1 << 12)
+#define PHY_MSP432_EPHYBMSR_10BTHD          (1 << 11)
+#define PHY_MSP432_EPHYBMSR_MFPRESUP        (1 << 6)
+#define PHY_MSP432_EPHYBMSR_ANC             (1 << 5)
+#define PHY_MSP432_EPHYBMSR_RFAULT          (1 << 4)
+#define PHY_MSP432_EPHYBMSR_ANEN            (1 << 3)
+#define PHY_MSP432_EPHYBMSR_LINKSTAT        (1 << 2)
+#define PHY_MSP432_EPHYBMSR_JABBER          (1 << 1)
+#define PHY_MSP432_EPHYBMSR_EXTEN           (1 << 0)
+
+#define PHY_MSP432_EPHYID1_OUIMSB           (0xFFFF)
+
+#define PHY_MSP432_EPHYID2_OUILSB_SHIFT     (10)
+#define PHY_MSP432_EPHYID2_OUILSB     (0x3F << PHY_MSP432_EPHYID2_OUILSB_SHIFT)
+#define PHY_MSP432_EPHYID2_VNDRMDL_SHIFT    (10)
+#define PHY_MSP432_EPHYID2_VNDRMDL    (0x3F << PHY_MSP432_EPHYID2_VNDRMDL_SHIFT)
+#define PHY_MSP432_EPHYID2_MDLREV           (0xF)
+
+#define PHY_MSP432_EPHYANA_NP               (1 << 15)
+#define PHY_MSP432_EPHYANA_RF               (1 << 13)
+#define PHY_MSP432_EPHYANA_ASMDUP           (1 << 11)
+#define PHY_MSP432_EPHYANA_PAUSE            (1 << 10)
+#define PHY_MSP432_EPHYANA_100BT4           (1 << 9)
+#define PHY_MSP432_EPHYANA_100BTXFD         (1 << 8)
+#define PHY_MSP432_EPHYANA_100BTX           (1 << 7)
+#define PHY_MSP432_EPHYANA_10BTFD           (1 << 6)
+#define PHY_MSP432_EPHYANA_10BT             (1 << 5)
+#define PHY_MSP432_EPHYANA_SELECT           (0x1F)
+
+#define PHY_MSP432_EPHYANLPA_NP             (1 << 15)
+#define PHY_MSP432_EPHYANLPA_ACK            (1 << 14)
+#define PHY_MSP432_EPHYANLPA_RF             (1 << 13)
+#define PHY_MSP432_EPHYANLPA_ASMDUP         (1 << 11)
+#define PHY_MSP432_EPHYANLPA_PAUSE          (1 << 10)
+#define PHY_MSP432_EPHYANLPA_100BT4         (1 << 9)
+#define PHY_MSP432_EPHYANLPA_100BTXFD       (1 << 8)
+#define PHY_MSP432_EPHYANLPA_100BTX         (1 << 7)
+#define PHY_MSP432_EPHYANLPA_10BTFD         (1 << 6)
+#define PHY_MSP432_EPHYANLPA_10BT           (1 << 5)
+#define PHY_MSP432_EPHYANLPA_SELECT         (0x1F)
+
+#define PHY_MSP432_EPHYANER_PDF             (1 << 4)
+#define PHY_MSP432_EPHYANER_LPNPABLE        (1 << 3)
+#define PHY_MSP432_EPHYANER_NPABLE          (1 << 2)
+#define PHY_MSP432_EPHYANER_PAGERX          (1 << 1)
+#define PHY_MSP432_EPHYANER_LPANABLE        (1 << 0)
+
+#define PHY_MSP432_EPHYANNPTR_NP            (1 << 15)
+#define PHY_MSP432_EPHYANNPTR_MP            (1 << 13)
+#define PHY_MSP432_EPHYANNPTR_ACK2          (1 << 12)
+#define PHY_MSP432_EPHYANNPTR_TOGTX         (1 << 11)
+#define PHY_MSP432_EPHYANNPTR_CODE          (0x7FF)
+
+#define PHY_MSP432_EPHYANLNPTR_NP           (1 << 15)
+#define PHY_MSP432_EPHYANLNPTR_ACK          (1 << 14)
+#define PHY_MSP432_EPHYANLNPTR_MP           (1 << 13)
+#define PHY_MSP432_EPHYANLNPTR_ACK2         (1 << 12)
+#define PHY_MSP432_EPHYANLNPTR_TOG          (1 << 11)
+#define PHY_MSP432_EPHYANLNPTR_CODE         (0x7FF)
+
+#define PHY_MSP432_EPHYCFG1_DONE            (1 << 15)
+#define PHY_MSP432_EPHYCFG1_TDRAR           (1 << 8)
+#define PHY_MSP432_EPHYCFG1_LLR             (1 << 7)
+#define PHY_MSP432_EPHYCFG1_FAMDIX          (1 << 6)
+#define PHY_MSP432_EPHYCFG1_RAMDIX          (1 << 5)
+#define PHY_MSP432_EPHYCFG1_FASTANEN        (1 << 4)
+#define PHY_MSP432_EPHYCFG1_FANSEL_SHIFT    (2)
+#define PHY_MSP432_EPHYCFG1_FANSEL     (0x3 << PHY_MSP432_EPHYCFG1_FANSEL_SHIFT)
+#define PHY_MSP432_EPHYCFG1_FRXDVDET        (1 << 1)
+
+#define PHY_MSP432_EPHYCFG2_FLUPPD          (1 << 6)
+#define PHY_MSP432_EPHYCFG2_EXTFD           (1 << 5)
+#define PHY_MSP432_EPHYCFG2_ENLEDLINK       (1 << 4)
+#define PHY_MSP432_EPHYCFG2_ISOMIILL        (1 << 3)
+#define PHY_MSP432_EPHYCFG2_RXERRIDLE       (1 << 2)
+#define PHY_MSP432_EPHYCFG2_ODDNDETDIS      (1 << 1)
+
+#define PHY_MSP432_EPHYCFG3_POLSWAP         (1 << 7)
+#define PHY_MSP432_EPHYCFG3_MDIMDIXS        (1 << 6)
+#define PHY_MSP432_EPHYCFG3_FLDWNM          (0x1F)
+
+#define PHY_MSP432_EPHYREGCTL_FUNC_SHIFT    (14)
+#define PHY_MSP432_EPHYREGCTL_FUNC     (0x3 << PHY_MSP432_EPHYREGCTL_FUNC_SHIFT)
+#define PHY_MSP432_EPHYREGCTL_FUNC_ADDRESS      (0x0)
+#define PHY_MSP432_EPHYREGCTL_FUNC_DATA_NO_INC  (0x1 << PHY_MSP432_EPHYREGCTL_FUNC_SHIFT)
+#define PHY_MSP432_EPHYREGCTL_FUNC_DATA_INC_RW  (0x2 << PHY_MSP432_EPHYREGCTL_FUNC_SHIFT)
+#define PHY_MSP432_EPHYREGCTL_FUNC_DATA_INC_WO  (0x3 << PHY_MSP432_EPHYREGCTL_FUNC_SHIFT)
+
+#define PHY_MSP432_EPHYADDAR_ADDRDATA       (0xFFFF)
+
+#define PHY_MSP432_EPHYSTS_MDIXM    (1 << 14)
+#define PHY_MSP432_EPHYSTS_RXLERR   (1 << 13)
+#define PHY_MSP432_EPHYSTS_POLSTAT  (1 << 12)
+#define PHY_MSP432_EPHYSTS_FCSL     (1 << 11)
+#define PHY_MSP432_EPHYSTS_SD       (1 << 10)
+#define PHY_MSP432_EPHYSTS_DL       (1 << 9)
+#define PHY_MSP432_EPHYSTS_PAGERX   (1 << 8)
+#define PHY_MSP432_EPHYSTS_MIIREQ   (1 << 7)
+#define PHY_MSP432_EPHYSTS_RF       (1 << 6)
+#define PHY_MSP432_EPHYSTS_JD       (1 << 5)
+#define PHY_MSP432_EPHYSTS_ANS      (1 << 4)
+#define PHY_MSP432_EPHYSTS_MIILB    (1 << 3)
+#define PHY_MSP432_EPHYSTS_DUPLEX   (1 << 2)
+#define PHY_MSP432_EPHYSTS_SPEED    (1 << 1)
+#define PHY_MSP432_EPHYSTS_LINK     (1 << 0)
+
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
+#define PHY_MSP432_EPHYSCR_
 /*****************************************************************************/
 /* Register values                                                           */
 /*****************************************************************************/
 
-/* the MMD provides register access into bare PHY chip. MMD provides its own
- * register address space, completely different between devices.
- * It should be implemented later.
- */
 
-#define PHY_MSP432_MMDACR_FUNC_SHIFT       14      /* ---DEF */
-#define MSP432_MMDACR_FUNC         (3 << MSP432_MMDACR_FUNC_SHIFT)
-#define MSP432_MMDACR_FUNC_ADDRESS     (0 << MSP432_MMDACR_FUNC_SHIFT)
-#define MSP432_MMDACR_FUNC_DATA        (1 << MSP432_MMDACR_FUNC_SHIFT)
-
-#define MSP432_MMDACR_DEVAD            (31 << 0)   /* ---DEF */
-#define MSP432_MMDACR_DEVAD_PCS        (3 << 0)    /* ---DEF */
-#define MSP432_MMDACR_DEVAD_ANN        (7 << 0)    /* ---DE- */
-#define MSP432_MMDACR_DEVAD_VENDOR     (30 << 0)   /* ---DE- */
-
-/* MSP432_MMDADR -----------------------------------------------------------*/
-
-/* MSP432_REVISION ---------------------------------------------------------*/
-/* note this register is valid only on original LAN8700 */
-
-/* MSP432_EDPDNLP ----------------------------------------------------------*/
-/* note this register is valid only on newer than original LAN8700 */
-
-#define MSP432_EDPDNLP_TXEN            (1 << 15)   /* ---DEF */
-
-#define MSP432_EDPDNLP_TXTIM_SHIFT     13      /* ---DEF */
-#define MSP432_EDPDNLP_TXTIM           (3<<MSP432_EDPDNLP_TXTIM_SHIFT)
-#define MSP432_EDPDNLP_TXTIM_1024MS        (0<<MSP432_EDPDNLP_TXTIM_SHIFT)
-#define MSP432_EDPDNLP_TXTIM_0768MS        (1<<MSP432_EDPDNLP_TXTIM_SHIFT)
-#define MSP432_EDPDNLP_TXTIM_0512MS        (2<<MSP432_EDPDNLP_TXTIM_SHIFT)
-#define MSP432_EDPDNLP_TXTIM_0256MS        (3<<MSP432_EDPDNLP_TXTIM_SHIFT)
-
-#define MSP432_EDPDNLP_RXWAKEEN        (1 << 12)   /* ---DEF */
-
-#define MSP432_EDPDNLP_RXTIM_SHIFT     10      /* ---DEF */
-#define MSP432_EDPDNLP_RXTIM           (3<<MSP432_EDPDNLP_RXTIM_SHIFT)
-#define MSP432_EDPDNLP_RXTIM_0064MS        (0<<MSP432_EDPDNLP_RXTIM_SHIFT)
-#define MSP432_EDPDNLP_RXTIM_0256MS        (1<<MSP432_EDPDNLP_RXTIM_SHIFT)
-#define MSP432_EDPDNLP_RXTIM_0512MS        (2<<MSP432_EDPDNLP_RXTIM_SHIFT)
-#define MSP432_EDPDNLP_RXTIM_1024MS        (3<<MSP432_EDPDNLP_RXTIM_SHIFT)
-
-#define MSP432_EDPDNLP_EXTCROSSOVER        (1 << 1)    /* ---DEF */
-#define MSP432_EDPDNLP_EXTMANCROSSOVER     (1 << 0)    /* ---DEF */
-
-/* MSP432_MCS --------------------------------------------------------------*/
-
-#define MSP432_MCS_EDPWRDOWN           (1 << 13)   /* ABCDEF */
-#define MSP432_MCS_LOWSQEN         (1 << 11)   /* A----- */
-#define MSP432_MCS_MDPREBP         (1 << 10)   /* A----- */
-#define MSP432_MCS_FARLOOPBACK         (1 << 9)    /* ABCDEF */
-#define MSP432_MCS_ALTINT          (1 << 6)    /* ABCDEF */
-#define MSP432_MCS_PHYADBP         (1 << 3)    /* A----- */
-#define MSP432_MCS_FORCELINK           (1 << 2)    /* A----- */
-#define MSP432_MCS_ENERGYON            (1 << 1)    /* ABCDEF */
-
-/* MSP432_SM ---------------------------------------------------------------*/
-
-/* @LAN8720A: Should write as 1 (only RMII supported) */
-/* @LAN8742A: Don't care bit on LAN8742A (only RMII supported) */
-#define MSP432_SM_MIIMODE          (1 << 14)   /* ABCDE- */
-#define MSP432_SM_MIIMODE_MII          (0 << 14)   /* ABCDE- */
-#define MSP432_SM_MIIMODE_RMII         (1 << 14)   /* ABCDE- */
-
-#define MSP432_SM_MODE_SHIFT           5       /* ABCDEF */
-#define MSP432_SM_MODE             (7 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_10HD            (0 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_10FD            (1 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_100HD           (2 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_100FD           (3 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_100HD_AN        (4 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_REPEATER        (5 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_POWERDOWN       (6 << MSP432_SM_MODE_SHIFT)
-#define MSP432_SM_MODE_ALL         (7 << MSP432_SM_MODE_SHIFT)
-
-#define MSP432_SM_PHYAD            (0x1F << 0) /* ABCDEF */
-
-/* MSP432_TDRDCR -----------------------------------------------------------*/
-
-/*this register is applicable only on @LAN8740A and @LAN8742A */
-
-#define MSP432_TDRDCR_DELAY            (15 << 0)   /* ---D-F */
-#define MSP432_TDRDCR_DELAY            (15 << 0)   /* ---D-F */
-
-#define MSP432_TDRDCR_LBCR_SHIFT       12      /* ---D-F */
-#define MSP432_TDRDCR_LBCR         (7 << MSP432_TDRDCR_LBCR_SHIFT)
-
-#define MSP432_TDRDCR_LPAT_SHIFT       6       /* ---D-F */
-#define MSP432_TDRDCR_LPAT         (7 << MSP432_TDRDCR_LPAT_SHIFT)
-
-#define MSP432_TDRDCR_HPAT_SHIFT       0       /* ---D-F */
-#define MSP432_TDRDCR_HPAT         (7 << MSP432_TDRDCR_HPAT_SHIFT)
-
-/* MSP432_TDRCSR -----------------------------------------------------------*/
-
-/*this register is applicable only on @LAN8740A and @LAN8742A */
-
-#define MSP432_TDRCSR_ENABLE           (1 << 15)   /* ---D-F */
-#define MSP432_TDRCSR_ADFILTER         (1 << 14)   /* ---D-F */
-
-#define MSP432_TDRCSR_COND_SHIFT       9       /* ---D-F */
-#define MSP432_TDRCSR_COND_DEFAULT     (0 << MSP432_TDRCSR_COND_SHIFT)
-#define MSP432_TDRCSR_COND_SHORTED     (1 << MSP432_TDRCSR_COND_SHIFT)
-#define MSP432_TDRCSR_COND_OPEN        (2 << MSP432_TDRCSR_COND_SHIFT)
-#define MSP432_TDRCSR_COND_MATCH       (3 << MSP432_TDRCSR_COND_SHIFT)
-
-#define MSP432_TDRCSR_STATUS           (1 << 8)    /* ---D-F */
-
-/* note for @LAN8742A the coefficients to meters are in table in DS page 55 */
-/* note for @LAN8740A the coefficients to meters are in table in DS page 58 */
-#define MSP432_TDRCSR_LENGTH           (15 << 0)   /* ---D-F */
-
-
-/* MSP432_ERRCNT -----------------------------------------------------------*/
-
-/* 16bit register not applicable for old @LAN8700 */
-
-/* MSP432_CSR --------------------------------------------------------------*/
-
-#define MSP432_CSR_AMDIXCTRL           (1 << 15)   /* ABCDEF */
-#define MSP432_CSR_CH_SELECT           (1 << 13)   /* ABCDEF */
-#define MSP432_CSR_SQEOFF          (1 << 11)   /* ABCDEF */
-#define MSP432_CSR_XPOL            (1 << 4)    /* ABCDEF */
-
-/* MSP432_CBLN --------------------------------------------------------------*/
-
-/* register applicable only on @LAN8740A and LAN8742A */
-
-#define MSP432_CBLN_CBLN_SHIFT         12      /* ---D-F */
-#define MSP432_CBLN_CBLN           (15 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_006M          (4 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_017M          (5 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_027M          (6 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_038M          (7 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_049M          (8 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_059M          (9 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_070M          (10 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_081M          (11 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_091M          (12 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_102M          (13 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_113M          (14 << MSP432_CBLN_CBLN_SHIFT)
-#define MSP432_CBLN_CBLN_123M          (15 << MSP432_CBLN_CBLN_SHIFT)
-
-
-/* MSP432_ISR --------------------------------------------------------------*/
-
-#define MSP432_ISR_WOLF            (1 << 8)    /* ---D-F */
-#define MSP432_ISR_ENERGYONF           (1 << 7)    /* ABCDEF */
-#define MSP432_ISR_ANCOMPF         (1 << 6)    /* ABCDEF */
-#define MSP432_ISR_RFAULTF         (1 << 5)    /* ABCDEF */
-#define MSP432_ISR_LINKDOWNF           (1 << 4)    /* ABCDEF */
-#define MSP432_ISR_ANLPACKF            (1 << 3)    /* ABCDEF */
-#define MSP432_ISR_PDFAULTF            (1 << 2)    /* ABCDEF */
-#define MSP432_ISR_ANPAGEF         (1 << 1)    /* ABCDEF */
-
-/* LAN8710_IMR --------------------------------------------------------------*/
-
-#define MSP432_IMR_WOLE            (1 << 8)    /* ---D-F */
-#define MSP432_IMR_ENERGYONE           (1 << 7)    /* ABCDEF */
-#define MSP432_IMR_ANCOMPE         (1 << 6)    /* ABCDEF */
-#define MSP432_IMR_RFAULTE         (1 << 5)    /* ABCDEF */
-#define MSP432_IMR_LINKDOWNE           (1 << 4)    /* ABCDEF */
-#define MSP432_IMR_ANLPACKE            (1 << 3)    /* ABCDEF */
-#define MSP432_IMR_PDFAULTE            (1 << 2)    /* ABCDEF */
-#define MSP432_IMR_ANPAGEE         (1 << 1)    /* ABCDEF */
-
-/* LAN8710_SCSR -------------------------------------------------------------*/
-
-#define MSP432_SCSR_AUTODONE           (1 << 12)   /* ABCDEF */
-
-#define MSP432_SCSR_GPO2           (1 << 9)    /* A----- */
-#define MSP432_SCSR_GPO1           (1 << 8)    /* A----- */
-#define MSP432_SCSR_GPO0           (1 << 7)    /* A----- */
-
-/* @LAN8742A: should write as 1 */
-/* @LAN8720A: should write as 1 */
-#define MSP432_SCSR_ENABLE4B5B         (1 << 6)    /* AB-DE- */
-
-#define MSP432_SCSR_SPEED_SHIFT        2       /* ABCDEF */
-#define MSP432_SCSR_SPEED          (7 << MSP432_SCSR_SPEED_SHIFT)
-#define MSP432_SCSR_SPEED_10HD         (1 << MSP432_SCSR_SPEED_SHIFT)
-#define MSP432_SCSR_SPEED_100HD        (2 << MSP432_SCSR_SPEED_SHIFT)
-#define MSP432_SCSR_SPEED_10FD         (5 << MSP432_SCSR_SPEED_SHIFT)
-#define MSP432_SCSR_SPEED_100FD        (6 << MSP432_SCSR_SPEED_SHIFT)
-
-#define MSP432_SCSR_NOSCRAMBLE         (1 << 0)    /* A----- */
 
 #endif /* PHY_MSP432E4_H */
