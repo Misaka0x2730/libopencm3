14ac82ab562b1d1691c01547eb4068e29ce3d867
Dmitry Rezvanov <dmitry.rezvanov@yandex.ru>
2018-10-26 12:46:45 +0400

    Initial commit

---
+++ include/libopencm3/msp432/e4/epi.h
@@ -0,0 +1,48 @@
+/** @defgroup epi_defines Defines
+ *
+ * @ingroup MSP432E4xx_defines
+ *
+ * @brief Defined Constants and Types for the MSP432E4 EPI
+ *
+ * @version 1.0.0
+ *
+ * @date TODO
+ *
+ * LGPL License Terms @ref lgpl_license
+ */
+
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2018 Dmitry Rezvanov <dmitry.rezvanov@yandex.ru>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MSP432E4_EPI_H
+#define MSP432E4_EPI_H
+
+/**@{*/
+
+#include <libopencm3/cm3/common.h>
+#include <libopencm3/msp432/e4/memorymap.h>
+#include <stdbool.h>
+
+BEGIN_DECLS
+
+END_DECLS
+
+/**@}*/
+
+#endif /* MSP432E4_EPI_H */
